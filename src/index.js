6d830a5fda094c39ef6b55fe7e084e37ac5d62db
Crazy24-x <aldiyar.toktar2003@gmail.com>
2025-01-29 14:37:24 +0500

    step6

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TodoList = () => {
-    const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li>{items[3]}</li>
-        </ul>
-    );
-};
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-const AppHeader = () =>{
-    return <h1>My To-do List</h1>;
-};
-
-const SearchPanel = () => {
-
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    // return <input placeholder="search" />;
-    return <input
-        style = {searchStyle}
-        placeholder= {searchText}
-        disabled={true} />;
-}
 const App = () => {
 
-    const value = '<script>alert ("")</script>';
-    // const isLoggedIn = false;
-    // const loginBox = <span>Log in to the system</span>;
-    // const welcomeBox = <span>Welcome Back</span>;
+
 
     return (
         <div>
-            {/*{ isLoggedIn ? null : loginBox }*/}
-            { value }
+
             <AppHeader />
             <SearchPanel />
             <TodoList/>
