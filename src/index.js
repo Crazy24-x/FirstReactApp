fd96fa0b4c39690209985c762720d4a8d210d51d
Crazy24-x <aldiyar.toktar2003@gmail.com>
2025-01-29 14:10:24 +0500

    step4

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const TodoList = () => {
 };
 
 const AppHeader = () =>{
-    return <h1>My Todo List</h1>;
+    return <h1>My To-do List</h1>;
 };
 const SearchPanel = () => {
     return <input placeholder="search" />;
